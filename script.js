e21351882da2380c1497287de35e9728942963cd
naksh08 <nakshatradeep21@gmail.com>
2024-07-30 21:37:34 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,51 @@
+const dynamicText = document.querySelector("h2 > span")
+const words = ["Good!","Tasty!","Amazing!","Savory!"]
+
+let wordIndex = 0
+let charIndex = 0
+let isDeleting = false
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex]
+    const currentChar = currentWord.substring(0,charIndex)
+    dynamicText.textContent = currentChar
+    dynamicText.classList.add("stop-blinking")
+
+    if(!isDeleting && charIndex < currentWord.length){
+        // typing the next character
+        charIndex++
+        setTimeout(typeEffect,200)
+    }
+    else if(isDeleting && charIndex>0){
+        // remove the previous character
+        charIndex--
+        setTimeout(typeEffect, 200)
+    }
+    else{
+        // when the word is deleted, switching to the next word
+        isDeleting = !isDeleting
+        dynamicText.classList.remove("stop-blinking")
+        wordIndex = !isDeleting ? (wordIndex+1) % words.length : wordIndex
+        setTimeout(typeEffect, 1200)
+    }
+}
+
+typeEffect()
+
+const button = document.querySelector(".btn")
+button.addEventListener("click", drawRipple)
+
+function drawRipple(event){
+    const x = event.clientX - event.target.offsetLeft
+    const y = event.clientY - event.target.offsetTop
+
+    const ripples = document.createElement("span")
+    ripples.style.left = x + "px"
+    ripples.style.top = y + "px"
+
+    this.appendChild(ripples)
+
+    setTimeout(()=>{
+        ripples.remove()
+    },1000)
+}
